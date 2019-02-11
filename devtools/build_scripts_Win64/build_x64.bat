f40cf614ed01c9246c791adb5ad5ed9258716226
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2019-02-11 19:38:24 +0100

    rm outdated Win scripts

--- devtools/build_scripts_Win64/build_x64.bat
+++
@@ -1,5 +0,0 @@
-ECHO Storing current directory in VSCMD_START_DIR
-SET "VSCMD_START_DIR=%CD%"
-CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-cd .build
-cmake --build . --config Release
