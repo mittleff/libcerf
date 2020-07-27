222df4b9a3f13e7675fe304a56af2306a59443b2
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2020-07-27 19:13:34 +0200

    test: replaced in/out arrays by one test command per in/out pair
    def edit( fn ):
        fnn = re.sub( r'^\./', '', fn )
        # print(fnn)
    
        # read in
        fd = open( fn, 'r' )
        ti = fd.read()
        fd.close
        t = ti
    
        pattern = r'\n\s*_cerf_cmplx z\[NTST\] = \{\s*?((C\(.+?,\s*?.+?\)(,\s*|\s*\};\s*))+)' + \
            r'\n\s*_cerf_cmplx w\[NTST\] = \{\s*?((C\(.+?,\s*?.+?\)(,\s*|\s*\};\s*))+)' + \
            r'.*?zTest\(.*?,.*?, (%s),.*?\);'
        zz = {}
        ww = {}
        names = []
        rr = re.compile(pattern % r'\w+')
        for m in re.finditer(rr, t):
            name = m.group(7)
            zz[name] = m.group(1)
            ww[name] = m.group(4)
            names.append(name)
        print("found names", names)
    
        def mysplit(s):
            ret = []
            for m in re.finditer(r'C\(.+?,\s*?.+?\)', s):
                ret.append(m.group(0))
            return ret
    
        for name in names:
            za = mysplit(zz[name])
            wa = mysplit(ww[name])
            if len(za)!=len(wa):
                raise Exception("array mismatch")
            out = ""
            for i in range(len(za)):
                out += "    ZTEST(result, 1e-13, %s(%s), %s);\n" % ( name, za[i], wa[i] )
            print("...", name, ":", len(za), "tests")
            rr = re.compile(pattern % name)
            t = re.sub(rr, out, t, 1)
    
        # save result
        if t==ti:
            return 0
        print( "Changed " + fn )
        fd = open( fn, 'w' )
        fd.write( t )
        fd.close
        return 1

--- test/testtool.h
+++ test/testtool.h
@@ -3,28 +3,28 @@ typedef struct {
     int total;
 } result_t;
 
-#define TEST_NEAR(result, limit, function_val, expected_val) \
-    ++result.total; \
-    if (fabs(function_val-expected_val)>limit*expected_val) { \
-        printf("failure in subtest %i: %s\n", result.total, #function_val); \
-        printf("- fct value %20.15g\n", function_val); \
-        printf("- expected  %20.15g\n", expected_val); \
-        printf("=> error %6.2g above limit %6.2g\n", \
-               fabs(function_val-expected_val), limit*expected_val); \
-        ++result.failed; \
+#define TEST_NEAR(result, limit, function_val, expected_val)                                       \
+    ++result.total;                                                                                \
+    if (fabs(function_val - expected_val) > limit * fabs(expected_val)) {                          \
+        printf("failure in subtest %i: %s\n", result.total, #function_val);                        \
+        printf("- fct value %20.15g\n", function_val);                                             \
+        printf("- expected  %20.15g\n", expected_val);                                             \
+        printf(                                                                                    \
+            "=> error %6.2g above limit %6.2g\n", fabs(function_val - expected_val),               \
+            limit* fabs(expected_val));                                                            \
+        ++result.failed;                                                                           \
     }
 
-#define ZTEST(result, limit, function_val, expected_val) \
-    ++result.total; \
-    if (fabs(creal(function_val-expected_val))>limit*creal(expected_val) || \
-        fabs(cimag(function_val-expected_val))>limit*cimag(expected_val)) {  \
-        printf("failure in subtest %i: %s\n", result.total, #function_val); \
-        printf("- fct value %20.15g + %20.15gi\n", creal(function_val), cimag(function_val)); \
-        printf("- expected  %20.15g + %20.15gi\n", creal(expected_val), cimag(expected_val)); \
-        printf("=> error %6.2g or %6.2g above limit %6.2g or %6.2g \n", \
-               fabs(creal(function_val-expected_val)), \
-               fabs(cimag(function_val-expected_val)), \
-               limit*creal(expected_val), \
-               limit*cimag(expected_val)); \
-        ++result.failed; \
+#define ZTEST(result, limit, function_val, expected_val)                                           \
+    ++result.total;                                                                                \
+    if (fabs(creal(function_val - expected_val)) > limit * fabs(creal(expected_val))               \
+        || fabs(cimag(function_val - expected_val)) > limit * fabs(cimag(expected_val))) {         \
+        printf("failure in subtest %i: %s\n", result.total, #function_val);                        \
+        printf("- fct value %20.15g%+20.15gi\n", creal(function_val), cimag(function_val));        \
+        printf("- expected  %20.15g%+20.15gi\n", creal(expected_val), cimag(expected_val));        \
+        printf(                                                                                    \
+            "=> error %6.2g or %6.2g above limit %6.2g or %6.2g \n",                               \
+            fabs(creal(function_val - expected_val)), fabs(cimag(function_val - expected_val)),    \
+            limit* fabs(creal(expected_val)), limit* fabs(cimag(expected_val)));                   \
+        ++result.failed;                                                                           \
     }
--- test/voigttest.c
+++ test/voigttest.c
@@ -3,20 +3,21 @@
 #include <math.h>
 #include <stdio.h>
 
-int main() {
-    result_t result = {0,0};
+int main()
+{
+    result_t result = {0, 0};
 
     // expected results analytically determined:
-    TEST_NEAR(result, 1e-15, voigt(0,1,0), 1/sqrt(6.283185307179586));
-    TEST_NEAR(result, 1e-15, voigt(0,0,1), 1/3.141592653589793);
-    TEST_NEAR(result, 1e-13, voigt(0,.5,.5), .41741856104074);
+    TEST_NEAR(result, 1e-15, voigt(0, 1, 0), 1 / sqrt(6.283185307179586));
+    TEST_NEAR(result, 1e-15, voigt(0, 0, 1), 1 / 3.141592653589793);
+    TEST_NEAR(result, 1e-13, voigt(0, .5, .5), .41741856104074);
 
     // expected results obtained from scipy.integrate:
-    TEST_NEAR(result, 1e-12, voigt(1,.5,.5), .18143039885260323);
-    TEST_NEAR(result, 1e-12, voigt(1e5,.5e5,.5e5), .18143039885260323e-5);
-    TEST_NEAR(result, 1e-12, voigt(1e-5,.5e-5,.5e-5), .18143039885260323e5);
-    TEST_NEAR(result, 1e-12, voigt(1,.2,5), 0.06113399719916219);
-    TEST_NEAR(result, 1e-12, voigt(1,5,.2), 0.07582140674553575);
+    TEST_NEAR(result, 1e-12, voigt(1, .5, .5), .18143039885260323);
+    TEST_NEAR(result, 1e-12, voigt(1e5, .5e5, .5e5), .18143039885260323e-5);
+    TEST_NEAR(result, 1e-12, voigt(1e-5, .5e-5, .5e-5), .18143039885260323e5);
+    TEST_NEAR(result, 1e-12, voigt(1, .2, 5), 0.06113399719916219);
+    TEST_NEAR(result, 1e-12, voigt(1, 5, .2), 0.07582140674553575);
 
     printf("%i/%i tests failed", result.failed, result.total);
     return result.failed ? 1 : 0;
