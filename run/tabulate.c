b75d7beb541292b21a4d901aac613e26f81df8c6
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2022-11-17 10:30:58 +0100

    add integer steps for 100/(1+x) to test each Chebychev polynomial

--- run/tabulate.c
+++ run/tabulate.c
@@ -26,7 +26,7 @@
 static const double R6[6] = { 1.0, 1.5, 2.2, 3.3, 4.7, 6.8 };
 
 void tabulate(double x) {
-    printf( "    RTEST(result, 1e-13, dawson(%9.1e), %24.15e);\n", x, dawson(x) );
+    printf( "    RTEST(result, 1e-13, im_w_of_x(%24.15e), %24.15e);\n", x, im_w_of_x(x) );
 }
 
 int main()
@@ -52,5 +52,13 @@ int main()
             tabulate(x);
         }
     }
+    printf("\n    // integer steps for 100/(1+x) to test each Chebychev polynomial\n");
+    for (int i=0; i<=101; ++i) {
+        printf("    // i=%i\n", i);
+        tabulate(100./(i+1e-13) - 1);
+        tabulate(100./(i+.5) - 1);
+        tabulate(100./(i+1-1e-13) - 1);
+    }
+
     return 0;
 }
