0aabf49897456ce5896c82624d3be605a5027163
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2024-09-16 17:13:49 +0200

    + run_erfcx

---
+++ run/run_erfcx.c
@@ -0,0 +1,41 @@
+/* Library libcerf:
+ *   Compute complex error functions, based on a new implementation of
+ *   Faddeeva's w_of_z. Also provide Dawson and Voigt functions.
+ *
+ * File run_erfcx.c:
+ *   Interactive evaluation of erfcx(x).
+ *
+ * Copyright:
+ *   (C) 2013 Forschungszentrum Jülich GmbH
+ *
+ * Licence:
+ *   Public domain.
+ *
+ * Author:
+ *   Joachim Wuttke, Forschungszentrum Jülich, 2024
+ *
+ * Website:
+ *   http://apps.jcns.fz-juelich.de/libcerf
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "cerf.h"
+
+IMPORT extern int faddeeva_algorithm;
+IMPORT extern int faddeeva_nofterms;
+
+int main( int argc, char **argv )
+{
+    if( argc!=2 ){
+        fprintf( stderr,  "usage:\n" );
+        fprintf( stderr,  "   run_erfcx x\n" );
+        exit(-1);
+    }
+
+    double x = atof( argv[1] );
+
+    double y = erfcx(x);
+    printf( "%21.16e %22.17e %3i %3i\n", x, y, faddeeva_algorithm, faddeeva_nofterms);
+    return 0;
+}
