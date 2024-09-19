f610ceb95aee8110e07d4cdf5f2aef1f3632951a
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2024-09-19 13:34:46 +0200

    mv high-precision functions to hp_funcs

--- devtool/pro_imwofx_chebcoeffs.py
+++ devtool/pro_imwofx_chebcoeffs.py
@@ -37,6 +37,7 @@
 
 from mpmath import *
 import functool as fut
+import hp_funcs as hp
 import random, sys
 
 mp.dps = 48
@@ -44,22 +45,12 @@ mp.pretty = True
 
 final = False # Extra checks, to be turned on in final production run
 
-def hp_imwx(x, doublecheck=False):
-    fz = exp(-x**2)*erfc(mpc(0, -x))
-    result = fz.imag
-    if doublecheck:
-        # Check mpmath-computed reference value against mpmath-based brute-force integration
-        r2 = 2/sqrt(pi) * exp(-x**2) * quad(lambda t : exp(t**2), [0, x])
-        if abs(result-r2)/result > 1e-17:
-            raise Exception(f"mpmath inaccurate")
-    return fz.imag
-
 if __name__ == '__main__':
 
     Nout = 8
 
     R = fut.octavicRanges(.5, 12., 6)
-    C = fut.chebcoef(R, Nout, hp_imwx, final)
+    C = fut.chebcoef(R, Nout, hp.imwx, final)
 
     # fut.print_cheby_coeffs(C)
     # fut.print_clenshaw_code(R, C, Nout)
