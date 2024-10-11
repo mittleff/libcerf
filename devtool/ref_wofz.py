76ace872669c20d9914c1eb3ba52b8c88e0f0311
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2024-10-11 09:24:50 +0200

    + test scripts ref_wofz, check1_wofz

---
+++ devtool/ref_wofz.py
@@ -0,0 +1,19 @@
+#!/bin/env python
+
+"""
+Print reference value of w(z) for one z.
+"""
+
+from mpmath import *
+import sys
+import hp_funcs as hp
+
+mp.dps = 48
+mp.pretty = True
+
+if __name__ == '__main__':
+    if len(sys.argv)<3:
+        print(f"Usage: {sys.argv[0]} <x> <y>")
+    z = mpc(sys.argv[1], sys.argv[2])
+    w = hp.wofz(z.real, z.imag)
+    print("%22.15e %22.15e" % (w.real, w.imag))
