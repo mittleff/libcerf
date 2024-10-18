725782078e46db5587ec5e9f4d2049cd102ecff7
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2024-10-18 09:39:03 +0200

    cleanup bisect_wofz; concentrate on problematic region

--- devtool/bisect_wofz.py
+++ devtool/bisect_wofz.py
@@ -27,50 +27,16 @@ rt.print_line = lambda locus, a, n, rr, f, f2: print('%2i %3i %3i  %21.16e  %21.
 
 
 if __name__ == '__main__':
-#     if len(sys.argv)<7:
-#         print(f"Usage: {sys.argv[0]} <fct> <range_mode> <N> <from> <to> <output_mode>")
-#         print(f"   where <fct>         is any of i (im_wofx) x (erfcx) xm(erfcx(-x))")
-#         print(f"   where <range_mode>  is any of l (lin) p(pos log) n(neg log)")
-#         print(f"   where <output_mode> is any of t (tabulate) w(print_worst)")
-#         sys.exit(-1)
-#     if sys.argv[1] == 'i':
-#         rt.external_program = "run/run_imwx"
-#         rt.f_hp = hp.imwx
-#     elif sys.argv[1] == 'x':
-#         rt.external_program = "run/run_erfcx"
-#         rt.f_hp = hp.erfcx
-#     else:
-#         raise Exception("Invalid fct")
-#     rt.range_mode = sys.argv[2]
-#     Ni = int(sys.argv[3])
-#     x_fr = float(sys.argv[4])
-#     x_to = float(sys.argv[5])
-#     rt.output_mode = sys.argv[6]
-#
-#     assert(x_fr < x_to)
-#
-#     if rt.range_mode == 'l':
-#         x_range = x_to-x_fr
-#         step = x_range/(Ni-1)
-#         X = [x_fr + i*step for i in range(Ni)]
-#     elif rt.range_mode == 'p':
-#         assert(0 < x_fr)
-#         step = log10(x_to/x_fr)/(Ni-1)
-#         X = [x_fr * 10**(i*step) for i in range(Ni)]
-#     elif rt.range_mode == 'n':
-#         assert(x_to < 0)
-#         step = log10(x_to/x_fr)/(Ni-1)
-#         X = [x_fr * 10**(i*step) for i in range(Ni)]
 
     rt.external_program = "run/run_wofz"
     rt.range_mode = 'p'
     rt.output_mode = 't'
 
-    N, fr, to = 500, 1e-3, 1e2
+    N, fr, to = 151, .2, 9
     step = log10(to/fr)/(N-1)
     S = [fr * 10**(i*step) for i in range(N)]
 
-    N, fr, to = 501, 1e-3, 1e2
+    N, fr, to = 151, .2, 9
     step = log10(to/fr)/(N-1)
     R = [fr * 10**(i*step) for i in range(N)]
 
@@ -83,4 +49,3 @@ if __name__ == '__main__':
         rt.f_hp = lambda r : hp.wofz(s, r)
         rt.scan_and_bisect(R)
         print()
-    #rt.print_conclusion()
