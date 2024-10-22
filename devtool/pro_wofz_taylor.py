2c608698f48929eb96265da3b8ff230b646935a8
Joachim Wuttke <j.wuttke@fz-juelich.de>
2024-10-22 18:54:29 +0200

    pro_wofz_taylor: backward yields w(z); check1: request doublecheck again

--- devtool/pro_wofz_taylor.py
+++ devtool/pro_wofz_taylor.py
@@ -23,16 +23,16 @@ def backward(z, M):
     w = z
     W = [0 for k in range(20)]
     for k in reversed(range(M)):
-        w = z - k/2/w
+        w = z - (k+1)/2/w
         if k<20:
             W[k] = mpc(0,1)/sqrt(pi)/w
+            print("bw %2i %22.15e %22.15e" % (k, W[k].real, W[k].imag))
     return W
 
 if __name__ == '__main__':
     z = mpc(4.5, 3.5)
-    Wf = forward(z)
+    Wa = forward(z)
     Wb = backward(z, 99)
-    Wc = backward(z, 199)
     for k in range(0,20):
-        print("%2i %22.15e %22.15e %22.15e  %22.15e %22.15e %22.15e" %
-              (k, Wf[k].real, Wb[k].real, Wc[k].real, Wf[k].imag, Wb[k].imag, Wc[k].imag))
+        print("%2i %22.15e %22.15e  %22.15e %22.15e" %
+              (k, Wa[k].real, Wb[k].real, Wa[k].imag, Wb[k].imag))
