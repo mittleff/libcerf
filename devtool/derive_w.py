198408ce1da55d4d28c8e1b7c9a869c06caac9de
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2024-11-08 17:50:11 +0100

    fwd with arg doublecheck

--- devtool/derive_w.py
+++ devtool/derive_w.py
@@ -15,34 +15,38 @@ mp.pretty = True
 def dw_integral(z, n):
     return mpc(0,1)**n * sqrt(2)**(n+1) / sqrt(pi) * exp(-z**2/2) * pcfu(n+1/2, mpc(0, -sqrt(2))*z)
 
-def forward(z, N):
+def forward(z, N, doublecheck=True):
     """
     Return Taylor coefficients T[k]=w^(k)/k!.
     """
     w2 = mpc(0, -1/sqrt(pi))
-    w1 = hp.wofz(z, False)
+    w1 = hp.wofz(z, doublecheck)
     T = [w1]
     for n in range(1, N):
         w1, w2 = -2*(z*w1 + w2)/n, w1
         T.append(w1)
-    if abs(T[-1] - dw_integral(z, N-1)) > 1e-17*abs(T[-1]):
-        raise Exception("w_n: double check failed")
+    if doublecheck:
+        r = dw_integral(z, N-1)
+        if abs(T[-1] - r) > 1e-17*abs(T[-1]):
+            print("# %25.16g %25.16g  %25.16g %25.16g  %8g" %
+                  (z.real, z.imag, T[-1].real, T[-1].imag, abs(T[-1] - r)/abs(T[-1])))
+            raise Exception("w_n: double check failed")
     return T
 
 if __name__ == '__main__':
     if len(sys.argv)==3:
         z = mpc(sys.argv[1], sys.argv[2])
-        T = forward(z, 60)
+        T = forward(z, 184, False)
         for n in range(len(T)):
             t = T[n]
             r = dw_integral(z, n)
+            if abs(t-r)/abs(t)>.5:
+                break
             q = 0
-            p = 0
-            if n>0:
-                p = abs(T[n])/abs(T[n-1])
-                q = abs(R[n])/abs(R[n-1])
-            print("%11.3e %11.3e %11.3e %8f  %11.3e %11.3e %11.3e %8f  %11.3e %11.3e" %
-                  (t.real, t.imag, abs(t), p, r.real, r.imag, abs(r), q, B[n].real, B[n].imag), )
+            if n>1:
+                q = abs(T[n]/T[n-1])
+            print("%2i %11.3e %11.3e %8e %8e %8.3f" %
+                  (n, t.real, t.imag, abs(t), abs(t-r)/abs(t), q) )
         sys.exit(0)
 
     N = 24
